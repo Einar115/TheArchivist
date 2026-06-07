deace344dcd5c2350cc46aa0fb54867fbe7ffaa7
Einar115 <einarnaim@hotmail.com>
2026-06-06 22:00:59 -0600

    chore: Initial structure.

---
+++ backend/src/main/java/com/einar115/thearchivist/TheArchivistApplication.java
@@ -0,0 +1,13 @@
+package com.einar115.thearchivist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheArchivistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheArchivistApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/einar115/thearchivist/TheArchivistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einar115.thearchivist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheArchivistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8807df2450645708a1585c37b9db80af53298932
Einar115 <einarnaim@hotmail.com>
2026-06-11 10:11:23 -0600

    feat: Add OpenApiConfig for Swagger API documentation setup

---
+++ backend/src/main/java/com/einar115/thearchivist/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.einar115.thearchivist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("The Archivist API")
+                        .version("1.0.0")
+                        .description("API RAG para consultar lore de videojuegos")
+                        .contact(new Contact()
+                                .name("Einar115")));
+    }
+}

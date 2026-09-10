a6b43497dc54808941aef2c7f8c1856ac1d426bf
Einar115 <einar115@outlook.com>
2026-09-10 16:18:48 -0600

    refactor: Update AuthService and AuthServiceImpl to support new authentication flow and user registration

--- backend/src/main/java/com/einar115/thearchivist/service/AuthService.java
+++ backend/src/main/java/com/einar115/thearchivist/service/AuthService.java
@@ -1,13 +1,13 @@
 package com.einar115.thearchivist.service;
 
 import com.einar115.thearchivist.dto.request.AuthRequest;
-import com.einar115.thearchivist.dto.request.RefreshRequest;
+import com.einar115.thearchivist.dto.request.UserRequest;
 import com.einar115.thearchivist.dto.response.AuthResponse;
-import com.einar115.thearchivist.dto.response.LogoutResponse;
-import com.einar115.thearchivist.dto.response.RefreshResponse;
+import com.einar115.thearchivist.dto.response.UserResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
-    AuthResponse login(AuthRequest authRequest);
-    RefreshResponse refreshToken(RefreshRequest refreshRequest);
-    LogoutResponse logout(RefreshRequest refreshRequest);
+    AuthResponse login(AuthRequest authRequest, HttpServletRequest req, HttpServletResponse res);
+    UserResponse register(UserRequest userRequest);
 }
